41b858fb93f8009578809ab1c9e40bfe98fc10f2
sainikhil-dev <mnikhilsai2000@gmail.com>
2021-05-25 22:33:28 +0530

    configkeys

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports={
-    MongoURI:'mongodb+srv://socialmediaapp:nikhil2000@socialmediaapp.ugmli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    GoogleClientID:'560062018068-fd075giaa3u1si47s9mvlp287vhnk90u.apps.googleusercontent.com',
-    GoogleClientSecret:'oKYDgw0cH1w3TSVUI1A4DsCQ'
+    MongoURI:process.env.MONGO_URI,
+    GoogleClientID:process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret:process.env.GOOGLE_CLIENT_SECRET
 }
